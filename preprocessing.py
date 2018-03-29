34eb2d7d03cd5e1bc01ffe2900f126fe5ee682ae
Kevin Lau <laukevin@uw.edu>
2018-03-29 16:40:57 -0700

    sdf

--- preprocessing.py
+++ preprocessing.py
@@ -70,5 +70,13 @@ for i in range(0, X.shape[0]):
 		FLCdict[X[i][0]].append([X[i][1], dates[i], D1[i], D2[i]])
 
 for i in FLCdict:
+    if FLCdict[i] !in treatDict:
+        del FLCdict[i]
+
+for i in treatDict:
+    if treatDict[i] !in FLCdict[i]:
+        del treatDict[i]
+
+for i in FLCdict:
 	temp = FLCdict[i]
 	FLCdict[i] = sorted(temp, key=lambda temp_entry: temp_entry[1])
